2b5fcd3874805237f04fcc28ae476f874c11587f
r taradepan <taradepan1313@gmail.com>
2022-07-10 10:49:49 +0530

    adding files

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import {MediumProvider} from '../context/MediumContext'
+
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <MediumProvider> 
+      <Component {...pageProps} />
+    </MediumProvider>
+  )
+}
+
+export default MyApp
